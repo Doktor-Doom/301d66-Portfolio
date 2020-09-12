f42ccd3719ea47fc245a085fcc2dfd34a4f50860
Doktor-Doom <andre.o.martin@gmail.com>
2020-09-12 13:35:14 -0700

    nodemon worshipping

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
+require('dotenv').config(); 
+
 const express = require('express')
 const app = express()
 const PORT = process.env.PORT;
-
-require('dotenv').config();
 
 app.use(express.static('./public'));
 
 app.get('/', function (req, res) {
   res.send('Whats\'s good, Hollywood?')
 });
- 
-// app.listen(3000)
 
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`)
 });
+
+// app.listen(3000)
